a27678b1765e7ee9d2d7ca28070bb810501b4e5f
Ibrahim <mohammedibrahim.sa1@gmail.com>
2022-09-04 18:26:00 +0530

    updated app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-const tracker_data = Vue.component('tracker_data', {
+
+const dashboard = Vue.component('dashboard', {
 
     template:
     `
@@ -27,14 +28,88 @@ const tracker_data = Vue.component('tracker_data', {
 				<a href='' type="button" class="btn btn-outline-dark">Delete</a>
 			</div>
 		</div>	
-    </div>   
+    </div>  
+    <div class="row gy-2 " style="width: 50rem; margin: auto; padding: 20px;">
+		<div class="col-2 ">
+			<a @click="addtracker" type="button" class="btn btn-light">Add new tracker</a>
+		</div>	
+	</div> 
     </div>
   `
+  ,methods:{
+      addtracker:function(){
+        this.$router.push({name:'new_tracker'})
+      }
+    } 
+})
+
+const new_tracker=Vue.component('new_tracker', {
+  template: `
+    <div>
+    <div class="container">
+
+    <div class="row py-2">
+      <div class="col">
+        <h2>Add Tracker</h2>
+      </div>
+    </div>
+
+
+  <form action="" method="POST" id="create_tracker">
+    <div class="row py-4 ">
+      <div class="col-1">
+        <p class="fs-6">Tracker type</p>
+      </div>
+      <div class="col-6">
+        <select name="tracker_type" class="form-select" aria-label="Default select example" required>
+            <option selected>Open this select menu</option>
+         
+          <option value="">tracker.name</option>
+          
+        </select>
+      </div>
+    </div>
+    <div class="row py-4">
+      <div class="col-1">
+        <p class="fs-6">Name</p>
+      </div>
+      <div class="col">
+        <div class="col-md-6">
+            <input class="form-control" name="tracker_name" type="text" placeholder="enter tracker name" aria-label="default input example" required>
+          </div>
+      </div>
+    </div>
+
+
+    <div class="row py-4">
+      <div class="col-1">
+        <p class="fs-6">Description</p>
+      </div>
+      <div class="col">
+        <div class="col-md-6">
+        <textarea class="form-control" v-model="desc" id="desc" rows="2" name="desc" placeholder="Write something..."></textarea>
+          </div>
+      </div>
+    </div>
+
     
+    <div class="row py-4 ">
+      <div class="col-6">
+        <button type="submit" class="btn btn-success">Submit</button>
+      </div>
+    </div>
+  </form>
+
+  </div>
+  
+  <div>
+  `
 })
 
 const routes= [
-  {path:'/',  component: tracker_data,}]
+  {path:'/',  component: dashboard, name:'dashboard'},
+  {path:'/newtracker', component: new_tracker, name:'new_tracker'} 
+]
 
 
 const router = new VueRouter({
